5e2f2a12afa2cb8b00c8bfd7eefdf9c49da8920b
rinaldojr7 <rinaldojunior77@gmail.com>
2024-08-21 21:38:40 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let nome = prompt("Digite o seu nome")
+let sobreNome = prompt("Digite o seu sobrenome")
+let idade = prompt("Digite sua idade")
+let anoAtual = 2024
+let nomeCompleto =(nome + sobreNome)
+let anoNascimento = (anoAtual - idade)
+
+console.log("seu nome é " + nome + " " + sobreNome)
+console.log("Seu ano de nascimento é " + anoNascimento)
